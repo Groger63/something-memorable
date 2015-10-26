90246e119ea1d9c31fee52657d78689acc8e1009
Groger63 <quentin.chambefort@gmail.com>
2015-10-26 15:09:36 +0000

    testejnf

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php 
-	echo "ok";
 require_once(__DIR__.'/config/config.php');
-echo "ok";
 //chargement autoloader pour autochargement des classes
 require_once(__DIR__.'/config/autoload.php');
-echo "ok";
 Autoload::load();
-echo "ok";
 
 //Lancement du front controller
 $cont = new frontController();
-echo "ok";
 ?>
